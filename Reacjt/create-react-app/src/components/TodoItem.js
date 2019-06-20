78022ff17da9ef6f25baa3cb3111a44373e45871
phubao1999 <phutuongbao1999@gmail.com>
2019-06-21 01:25:36 +0700

    Components

---
+++ Reacjt/create-react-app/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    render(){
+        return (
+            <div className="TodoItem">
+                <p>Go To Play Soccer</p>
+            </div>
+        );
+    }
+} 
+
+export default TodoItem;
