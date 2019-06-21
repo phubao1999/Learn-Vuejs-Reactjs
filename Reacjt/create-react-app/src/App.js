81ce50e86bfdde06282b8d5b43044f259de60a9c
phubao1999 <phutuongbao1999@gmail.com>
2019-06-21 19:40:40 +0700

    aaa

--- Reacjt/create-react-app/src/App.js
+++ Reacjt/create-react-app/src/App.js
@@ -8,7 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem />
+        <TodoItem title="Go Play Soccer" />
+        <TodoItem title="English" />
+        <TodoItem title="Gay" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
