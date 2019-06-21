b9f5aa1a4884d277569c0a78dfc995660c918e06
phubao1999 <phutuongbao1999@gmail.com>
2019-06-22 00:02:03 +0700

    Render A list By Map Method

--- Reacjt/create-react-app/src/App.js
+++ Reacjt/create-react-app/src/App.js
@@ -1,30 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TodoItem title="Go Play Soccer" />
-        <TodoItem title="English" />
-        <TodoItem title="Gay" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.todoItems = [
+      'Go Play Soccer',
+      'English',
+      'Gay'
+    ];
+  }
+  render(){
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item, index) => <TodoItem key={index} title={item} />)
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
